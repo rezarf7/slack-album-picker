9ba109726b41082ffc091e86890178adf7970cc9
rezarf7 <frazer@parkerz.com>
2026-03-15 07:43:24 +1100

    Update weeklyPick.js

--- slack-album-picker/src/jobs/weeklyPick.js
+++ slack-album-picker/src/jobs/weeklyPick.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
+const db = require("../db");
 const { app } = require("../slack");
 const { runNewPick } = require("../picker");
 
 (async () => {
-  await runNewPick({
-    client: app.client,
-    channelId: process.env.SLACK_CHANNEL_ID
-  });
+  try {
+    await db.init();
 
-  console.log("Weekly pick done");
-  process.exit(0);
+    await runNewPick({
+      client: app.client,
+      channelId: process.env.SLACK_CHANNEL_ID
+    });
+
+    console.log("Weekly pick done");
+    process.exit(0);
+  } catch (err) {
+    console.error("weeklyPick failed:", err);
+    process.exit(1);
+  }
 })();
